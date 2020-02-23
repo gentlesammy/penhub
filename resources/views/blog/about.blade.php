03d9b1df741cd5e81369622091e5888ce351b286
gentlesammy <psalmsam84@gmail.com>
2020-02-23 03:39:06 -1100

    switched over to mysql

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -12,5 +12,26 @@
 
 
 @section('content')
-    <h1>ABOUT US</h1>
+   <div class="container">
+       <div class="row">
+           <div class="col-md-6 bg-primary text-white text-center">
+            <div class="imgcont" style="width:400px; height:400px; border-radius:50%; overflow:hidden;">
+                <img src="img/samodunladeOpeyemi.jpeg" alt="" class="img img-fluid img-bordered">
+              </div>
+            <h1 class="pt-5">OPEYEMI MI OWON</h1>
+            <p class="pb-5">I just want to tell you that i missssss you,
+                i cant wait to see you back at home
+                kisseeeeeeeeeeeeeeeesssssssssssssss
+            </p>
+           </div>
+
+       </div>
+   </div>
+
+
+
+
+
+
+
 @endsection
