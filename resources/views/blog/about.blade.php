664d433a939dc4943aae2fe41368ea49cd907269
gentlesammy <psalmsam84@gmail.com>
2020-02-23 03:43:46 -1100

    switched over to mysql

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -15,10 +15,10 @@
    <div class="container">
        <div class="row">
            <div class="col-md-6 bg-primary text-white text-center">
-            <div class="imgcont" style="width:400px; height:400px; border-radius:50%; overflow:hidden;">
-                <img src="img/samodunladeOpeyemi.jpeg" alt="" class="img img-fluid img-bordered">
+            <div class="imgcont" style="width:300px; height:300px; border-radius:50%; overflow:hidden;">
+                <img src="img/samodunladeOpeyemi.jpeg" alt="" class="">
               </div>
-            <h1 class="pt-5">OPEYEMI MI OWON</h1>
+            <h1 class="">OPEYEMI MI OWON</h1>
             <p class="pb-5">I just want to tell you that i missssss you,
                 i cant wait to see you back at home
                 kisseeeeeeeeeeeeeeeesssssssssssssss
